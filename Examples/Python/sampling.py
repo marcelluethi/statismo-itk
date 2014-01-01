cf45511737cb81313ad6654b385823fb29a69042
Marcel Luethi <marcel.luethi@unibas.ch>
2014-01-01 22:46:07 +0100

    added sampling example

---
+++ Examples/Python/sampling.py
@@ -0,0 +1,40 @@
+#
+# This file is part of the statismo library.
+#
+# Author: Marcel Luethi (marcel.luethi@unibas.ch)
+#
+#Copyright (c) 2013 University of Basel
+# All rights reserved.
+#
+#
+# This example shows how samples can be drawn from a model, using the Python interface of statismo-itk.
+#
+import sys
+import statismo_ITKPython as sitk
+import os.path
+
+
+def main(modelname, resdir): 
+
+    rep = sitk.itkStandardMeshRepresenterF3.New()
+    model = sitk.itkStatisticalModelMF3.New()
+    model.Load(rep, modelname)
+
+    mean = model.DrawMean()
+    writer = sitk.itkMeshFileWriterMF3.New(mean, FileName=os.path.join(resdir, "mean.vtk"))
+    writer.Update()
+    
+
+    for i in xrange(0, 10):
+        sample = model.DrawSample()
+        writer = sitk.itkMeshFileWriterMF3.New(sample, FileName=os.path.join(resdir, "sample-%d.vtk"%i))
+        writer.Update()
+        
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print "usage: %s model resdir" 
+        sys.exit(-1)
+
+    main(*sys.argv[1:])
